39de5a0e3518ee73d0d3a1705740408bd2bd5a24
SD-Dikov <s.d.dikov@gmail.com>
2019-06-26 21:28:34 +0300

    Исправлены имена аргументов

--- js/main.js
+++ js/main.js
@@ -5,18 +5,18 @@ var placeType = ['palace', 'flat', 'house', 'bungalo'];
 var mapBlock = document.querySelector('.map');
 var pinList = document.querySelector('.map__pins');
 
-var getAdList = function (avatarNumbers, placeType) {
+var getAdList = function (imageNumbers, typeList) {
   var adList = [];
   var randomInteger = function (min, max) {
     return Math.floor(min + Math.random() * (max + 1 - min));
   };
-  for (var i = 0; i < avatarNumbers.length; i++) {
+  for (var i = 0; i < imageNumbers.length; i++) {
     var locationX = randomInteger(50, 1100);
     var locationY = randomInteger(130, 630);
-    var randomTypeValue = randomInteger(0, placeType.length - 1);
+    var randomTypeValue = randomInteger(0, typeList.length - 1);
     adList.push({
-      'author': {'avatar': 'img/avatars/user0' + avatarNumbers[i] + '.png'},
-      'offer': {'type': placeType[randomTypeValue]},
+      'author': {'avatar': 'img/avatars/user0' + imageNumbers[i] + '.png'},
+      'offer': {'type': typeList[randomTypeValue]},
       'location': {'x': locationX, 'y': locationY}
     });
   }
@@ -25,20 +25,20 @@ var getAdList = function (avatarNumbers, placeType) {
 
 var adList = getAdList(avatarNumbers, placeType);
 
-var renderPin = function (adList, i) {
+var renderPin = function (dataList, i) {
   var pinTamplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var pinElement = pinTamplate.cloneNode(true);
-  pinElement.style.left = adList[i].location.x - 25 + 'px';
-  pinElement.style.top = adList[i].location.y - 70 + 'px';
-  pinElement.querySelector('img').src = adList[i].author.avatar;
-  pinElement.querySelector('img').alt = adList[i].offer.type;
+  pinElement.style.left = dataList[i].location.x - 25 + 'px';
+  pinElement.style.top = dataList[i].location.y - 70 + 'px';
+  pinElement.querySelector('img').src = dataList[i].author.avatar;
+  pinElement.querySelector('img').alt = dataList[i].offer.type;
 
   return pinElement;
 };
-var getPinsFragment = function (adList) {
+var getPinsFragment = function (dataList) {
   var pinsFragment = document.createDocumentFragment();
-  for (var i = 0; i < adList.length; i++) {
-    pinsFragment.appendChild(renderPin(adList, i));
+  for (var i = 0; i < dataList.length; i++) {
+    pinsFragment.appendChild(renderPin(dataList, i));
   }
   return pinsFragment;
 };
