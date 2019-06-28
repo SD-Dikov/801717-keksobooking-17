02de481851167e672efada826017c432551d8974
SD-Dikov <s.d.dikov@gmail.com>
2019-06-28 12:20:52 +0300

    Добавленны комментарии

--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@ var avatarNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
 var placeType = ['palace', 'flat', 'house', 'bungalo'];
 var mapBlock = document.querySelector('.map');
 var pinList = document.querySelector('.map__pins');
-
+// Функция создания массива объектов со случайными координатами, случайным типом жилья и аватарами
 var getAdList = function (imageNumbers, typeList) {
   var adList = [];
+  // Функция нахождения случаного числа в диапазоне
   var randomInteger = function (min, max) {
     return Math.floor(min + Math.random() * (max + 1 - min));
   };
+  // цикл, в теле которого, в массив добавляются данные, количество циклов ограниченно количеством файлов с аватарами
   for (var i = 0; i < imageNumbers.length; i++) {
     var locationX = randomInteger(50, 1100);
     var locationY = randomInteger(130, 630);
@@ -24,7 +26,7 @@ var getAdList = function (imageNumbers, typeList) {
 };
 
 var adList = getAdList(avatarNumbers, placeType);
-
+// функция создания метки на основе шаблона и заполнения ее данными
 var renderPin = function (dataList, i) {
   var pinTamplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var pinElement = pinTamplate.cloneNode(true);
@@ -35,8 +37,10 @@ var renderPin = function (dataList, i) {
 
   return pinElement;
 };
+// функция создания фрагмента, состоящего из заполненных данными шаблонов меток
 var getPinsFragment = function (dataList) {
   var pinsFragment = document.createDocumentFragment();
+  // цикл, на каждой итерации которого, вызывается функция создания метки на основе шаблона и заполнения ее данными, количество циклов ограниченно длиной массива с данными
   for (var i = 0; i < dataList.length; i++) {
     pinsFragment.appendChild(renderPin(dataList, i));
   }
@@ -44,5 +48,5 @@ var getPinsFragment = function (dataList) {
 };
 
 mapBlock.classList.remove('map--faded');
-
+// добавление созданного фрагмента в разметку
 pinList.appendChild(getPinsFragment(adList));
